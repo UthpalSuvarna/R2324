fd8b3a4bf4a90659fce73449329bd7f673a4415b
UthpalSuvarna <uthpal33@gmail.com>
2023-11-06 21:32:55 +0530

    Improved Herosection

--- app/src/components/Herosection.jsx
+++ app/src/components/Herosection.jsx
@@ -1,14 +1,23 @@
-import heroImage from '../assets/image.png';
+import heroImage from "../assets/image.png";
 export default function Herosection() {
-    return(
-        <section id="herosection" className=" flex items-center justify-around h-screen bg-stone-800">
-            <div className="m-20 p-5">
-                <h1 className="text-emerald-300 text-5xl">Welocme to the world of FOSS</h1>
-                <p className=' text-emerald-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-            </div>
-            <div className='flex items-center justify-center'>
-                <img src={heroImage} alt="image" className=' w-3/4' />
-            </div>
-        </section>
-    );
+  return (
+    <section
+      id="herosection"
+      className="flex justify-center bg-slate-950"
+    >
+      <div className="flex items-center justify-around min-h-screen  max-w-screen-2xl">
+        <div className="p-5 m-10">
+          <h1 className="text-teal-200 text-5xl font-bold">
+            Welcome to the world of <br/><span>FOSS</span>
+          </h1>
+          <p className=" text-emerald-300 mt-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quia dicta et ipsum iste in quis dolorum omnis ea pariatur?
+          </p>
+        </div>
+        <div className="flex items-center justify-center">
+          <img src={heroImage} alt="image" className=" w-3/4" />
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="bg-stone-700  h-16 content-center p-4 fixed top4 w-screen z-50">
+    <nav className=" bg-slate-900  h-16 content-center p-4 fixed top4 w-screen z-50">
       <div className=" text-emerald-300 text-xl mx-4  font-sans font-bold">
       R2324
       </div>
