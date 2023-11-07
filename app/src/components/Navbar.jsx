502bc04f36f76cf25df81867287caa5f67b0f25a
UthpalSuvarna <uthpal33@gmail.com>
2023-11-07 21:58:15 +0530

    Changes in Navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 export default function Navbar() {
   return (
-    <nav className=" bg-slate-900  h-16 content-center p-4 fixed top4 w-screen z-50">
-      <div className=" text-emerald-300 text-xl mx-4  font-sans font-bold">
-      R2324
+    <div className="flex justify-center bg-slate-950  h-16 p-4 fixed top4 w-screen z-50">
+      <div className="flex justify-between w-4/6">
+        <div className=" text-emerald-300 tex-xl mx-4  font-sans font-bold">
+          R2324
+        </div>
+        <nav>
+          <ul className="flex text-white">
+            <li className="mx-6">Home</li>
+            <li className="mx-6">FOSS</li>
+            <li className="mx-6">GNU/LInux</li>
+          </ul>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 }
