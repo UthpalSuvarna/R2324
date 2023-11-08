118c8889abbed4ab6f957099851fb7b50b007bf5
UthpalSuvarna <uthpal33@gmail.com>
2023-11-08 18:18:53 +0530

    Added Sidebar

--- app/src/pages/Homepage.jsx
+++ app/src/pages/Homepage.jsx
@@ -1,17 +1,19 @@
 import Fossinfo from "../components/Fossinfo";
 import GNUinfo from "../components/GNUinfo";
 import Herosection from "../components/Herosection";
-import Navbar from "../components/Navbar";
+import SideNav from "../components/Sidebar";
 import Timeline from "../components/Timeline";
 
-export default function Homepage () {
-    return(
-        <>
-        <Navbar/>
-        <Herosection/>
-        <Fossinfo/>
-        <GNUinfo/>
-        <Timeline/>
-        </>
-    )
+export default function Homepage() {
+  return (
+    <div className="flex">
+      <SideNav />
+      <div>
+        <Herosection />
+        <Fossinfo />
+        <GNUinfo />
+        <Timeline />
+      </div>
+    </div>
+  );
 }
